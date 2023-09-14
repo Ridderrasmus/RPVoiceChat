4415107390fe9adb116aebf588d59d3b287e0f75
Rasmus Tanggaard <rasmus@tthost.dk>
2023-09-14 08:10:33 +0200

    Started work on recipe disabling

---
+++ src/Utils/RecipeHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RPVoiceChat.Utils
+{
+    public class RecipeHandler
+    {
+
+        public Dictionary<string, List<string>> itemCodeToRecipes = new Dictionary<string, List<string>>()
+        {
+            { "item-callbell", new List<string> { "callbell" }  }
+        };
+    }
+}
