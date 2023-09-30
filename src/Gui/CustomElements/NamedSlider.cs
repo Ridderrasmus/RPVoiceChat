c1d28ef4428666c6fbf4821be35316f5aad8f7c9
Dmitry221060 <ya.dima42103ya@yandex.ru>
2023-09-30 20:49:08 +0500

    Add Named Slider

---
+++ src/Gui/CustomElements/NamedSlider.cs
@@ -0,0 +1,21 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace RPVoiceChat.Gui
+{
+    public class NamedSlider : GuiElementSlider
+    {
+        public string name;
+
+        private NamedSlider(ICoreClientAPI capi, string name, ActionConsumable<int> callback, ElementBounds bounds) : base(capi, callback, bounds)
+        {
+            this.name = name;
+        }
+
+        public static NamedSlider Create(ICoreClientAPI capi, string name, ActionConsumable<int, string> extendedCallback, ElementBounds bounds)
+        {
+            ActionConsumable<int> baseCallback = val => extendedCallback(val, name);
+            return new NamedSlider(capi, name, baseCallback, bounds);
+        }
+    }
+}
